064c478b1db7f358173d40ffcedccbe4d66a4886
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-31 16:06:29 +0300

    //

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,9 +4,15 @@ class CardList extends Component {
     render() {
         console.log(this.props.monsters);
         return (
-            <div>
+            <div className='card-list'>
                 {this.props.monsters.map((monster) => {
-                    return <div key={monster.id}><h1>{monster.name}</h1></div>
+                    const {name, email, id} = monster // Destruct name, email, id so there is no need to write monster.id|email|name.
+                    return <div key={id}>
+                        <img alt={`monster ${name}`}
+                             src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
                 })}
             </div>
         )
@@ -14,3 +20,5 @@ class CardList extends Component {
 }
 
 export default CardList;
+
+// return <div key={monster.id}><h1>{monster.name}</h1></div>
